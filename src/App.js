a0e79deac7f86f1f80d66370e918802d7de95477
Modyaly21 <malyhas2020@gmail.com>
2023-11-29 18:33:17 +0200

    OLP-19-Make-controller-for-backend version2 with routing with other pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import './App.css';
 import {ElearningPage,ProfileImage, Header,UserInfo,SideBar,EnrolledCourses,MutualFriends,EnrolledCP,Signup,Login} from "./components/index"
 import {UpdateProfile} from "./components/index"
+import {BrowserRouter,Route, Routes} from "react-router-dom"
 function App() {
   
   return (
     <>
-    <Signup/>
-      {/* <ElearningPage>
-        <Header/>
-        <SideBar/>
-        <ProfileImage position={"Instructor"} name={"Mohamed Elnady"}/>
-        <UserInfo information={
-          {name : "Ziad Aly",
-          dateofbirth : "20/5/2011",
-          gender:"Male",
-          school:"EL-Ekhlas",
-          degree:"PHD"}
-          }/>
-        <EnrolledCP>
-          <EnrolledCourses/>
-          <MutualFriends/>
-        </EnrolledCP>
-        <UpdateProfile/>
+      <BrowserRouter>
+        <Routes>
+          <Route index  element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/updateprofile" element={
+          <>
+            <Header/>
+            <SideBar/>
+            <UpdateProfile/>
+          </>
+          } />
+          <Route path="/profile" element={
+            
+            <ElearningPage>
+              <Header/>
+              <SideBar/>
+              <ProfileImage position={"Instructor"} name={"Mohamed Elnady"}/>
+              <UserInfo information={
+                {name : "Ziad Aly",
+                dateofbirth : "20/5/2011",
+                gender:"Male",
+                school:"EL-Ekhlas",
+                degree:"PHD"}
+                }/>
+              <EnrolledCP>
+              <EnrolledCourses/>
+              <MutualFriends/>
+            </EnrolledCP>
+            
         
-      </ElearningPage> */}
+        </ElearningPage>
+          } />
+          
+        </Routes>
+      </BrowserRouter>
     </>
+      
+
   );
 }
 
