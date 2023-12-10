76a7e4059ac4b0cade2eef48eaa19745023820c4
moeelnady <mohamedelnady007@gmail.com>
2023-12-11 01:46:20 +0200

    more features for course addition

--- backend/src/main/java/com/example/demo/course/CourseController.java
+++ backend/src/main/java/com/example/demo/course/CourseController.java
@@ -1,11 +1,15 @@
 package com.example.demo.course;
 
+import com.example.demo.instructor.Instructor;
+import com.example.demo.instructor.InstructorService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
     @Autowired
     private CourseService courseService;
+    @Autowired
+    private InstructorService instructorService;
 
     @PostMapping("/addCourse")
     public String addCourse(@RequestBody Course course){
@@ -20,9 +26,33 @@ public class CourseController {
         if(courseService.getCourseById(course.getCourseCode())!=null){
             return "the course already exists";
         }
-        Course addedCourse=  courseService.savaCourseDetails(course);
+        Course addedCourse=  courseService.saveCourseDetails(course);
 
         return "course has been added successfully";
+    }
+    @GetMapping("/getAll")
+    public List<Course> getInstructorCourses(@RequestBody Instructor instructor){
+        List<Course> allCourses = courseService.getAllCourses();
+        Instructor instructorFromDataBase= instructorService.findByEmail(instructor.getEmail());
+        List<Course> filteredCourses = new ArrayList<>();
+        for(Course c :allCourses){
+            if(c.getInstructorId() == instructorFromDataBase.getId()){
+                filteredCourses.add(c);
+            }
+        }
+        return filteredCourses;
+    }
+    @PostMapping("/deleteCourse")
+    public String deleteCourse(@RequestBody Course course){
+        ///TODO : USER AUTHENTICATION
+        if(courseService.getCourseById(course.getCourseCode())==null){
+            return "the course doesn't exists";
+        }
+        return courseService.deleteCourse(course);
+    }
+    @GetMapping("/sortedCourses")
+    public List<Course> getSortedCriteriaCourses(@RequestBody String criteria){
+        return courseService.sortBy(criteria);
     }
 
 }
--- backend/src/main/java/com/example/demo/course/CourseService.java
+++ backend/src/main/java/com/example/demo/course/CourseService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class CourseService {
     @Autowired
     CourseRepository courseRepository;
-    public Course savaCourseDetails(Course course){
+    public Course saveCourseDetails(Course course){
         return courseRepository.save(course);
     }
     public List<Course> getAllCourses(){
@@ -18,4 +18,23 @@ public class CourseService {
     public Course getCourseById(String courseCode){
         return courseRepository.findById(courseCode).orElse(null);
     }
+    public String deleteCourse(Course course){
+        courseRepository.deleteById(course.getCourseCode());
+        return "deleted Successfully the course ";
+    }
+    public List<Course> sortBy(String criteria){
+        List<Course> courses = courseRepository.findAll();
+        if(criteria.equalsIgnoreCase("courseName")){
+            courses.sort((o1, o2) -> o1.getCourseName().compareTo(
+                    o2.getCourseName()));
+            return courses;
+        }
+        else if(criteria.equalsIgnoreCase("deadline")){
+            courses.sort((o1, o2) -> o1.getDeadLine().compareTo(
+                    o2.getDeadLine()));
+            return courses;
+        }
+        return null;
+    }
+
 }
