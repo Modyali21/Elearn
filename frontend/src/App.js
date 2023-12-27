b03d913475a3fa48decdc8d435e7658156542f4e
Modyaly21 <malyhas2020@gmail.com>
2023-12-27 19:46:25 +0200

    OLP-60 Added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,97 @@
-import './App.css';
-import {ElearningPage,Header,CourseEnroll,MakeCourse,Signup,Login,StudentProfile, Course,MakeLecture} from "./components/index"
-import {UpdateProfile} from "./components/index"
-import {Admin} from "./components/index"
-import {BrowserRouter,Route, Routes} from "react-router-dom"
+import "./App.css";
+import {
+  ElearningPage,
+  Header,
+  CourseEnroll,
+  MakeCourse,
+  Signup,
+  Login,
+  StudentProfile,
+  Course,
+  MakeLecture,
+  HomePage,
+} from "./components/index";
+import { UpdateProfile } from "./components/index";
+import { Admin } from "./components/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CookiesProvider, useCookies } from "react-cookie";
 function App() {
-
   const [cookies, setCookie] = useCookies(["user"]);
 
   function handleLogin(user) {
     setCookie("user", user, { path: "/" });
   }
   return (
-
-    <> 
+    <>
       <BrowserRouter>
         <Routes>
+          <Route
+            path="/elearning"
+            element={
+              <>
+                <Header />
+                <HomePage />
+              </>
+            }
+          />
           <Route path="/course" element={<Course />} user={cookies.user} />
-          <Route path="/courseenroll" element={<CourseEnroll user={cookies.user} />} />
-          <Route path="/makecourse" element={<MakeCourse user={cookies.user} />} />
-          <Route path="/makelecture" element={<MakeLecture user={cookies.user} />} />
-          <Route path="/admin" element={<><Header/><Admin user={cookies.user} /></>} />
-          <Route index  element={
-            <CookiesProvider>
+          <Route
+            path="/courseenroll"
+            element={<CourseEnroll user={cookies.user} />}
+          />
+          <Route
+            path="/makecourse"
+            element={<MakeCourse user={cookies.user} />}
+          />
+          <Route
+            path="/makelecture"
+            element={<MakeLecture user={cookies.user} />}
+          />
+          <Route
+            path="/admin"
+            element={
+              <>
+                <Header />
+                <Admin user={cookies.user} />
+              </>
+            }
+          />
+          <Route
+            index
+            element={
+              <CookiesProvider>
                 <div>
                   {cookies.user || !cookies.user ? (
                     <Login onLogin={handleLogin} />
-                    ) : (
-                  <></>
+                  ) : (
+                    <></>
                   )}
                 </div>
-            </CookiesProvider>
-          } />
+              </CookiesProvider>
+            }
+          />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/updateprofile" element={
-          <>
-            <Header/>
-            <UpdateProfile user={cookies.user}/>
-          </>
-          } />
-          <Route path="/profile" element={
-            <ElearningPage>
-              <Header/>
-              <StudentProfile user={cookies.user}/>
-            </ElearningPage >
-          }/>
-          </Routes>
+          <Route
+            path="/updateprofile"
+            element={
+              <>
+                <Header />
+                <UpdateProfile user={cookies.user} />
+              </>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ElearningPage>
+                <Header />
+                <StudentProfile user={cookies.user} />
+              </ElearningPage>
+            }
+          />
+        </Routes>
       </BrowserRouter>
-      
     </>
-      
-
   );
 }
 
